afd53855abbf5d5fe60c98babc552de1de93638b
ser1zw <azariahsawtikes@gmail.com>
2013-12-30 23:37:04 +0900

    add ruby-electric-mode

--- files/.emacs.d/config/60-ruby.el
+++ files/.emacs.d/config/60-ruby.el
@@ -5,3 +5,13 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
+(add-to-list 'load-path (get-latest-package-dir "ruby-electric"))
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(defun ruby-insert-end () 
+  "Insert \"end\" at point and reindent current line." 
+  (interactive) 
+  (insert "end") 
+  (ruby-indent-line t) 
+  (end-of-line))
+
