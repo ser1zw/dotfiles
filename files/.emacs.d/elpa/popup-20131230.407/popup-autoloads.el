a6a7df913a3c8d4c0ed37d53092d06078d62c408
ser1zw <azariahsawtikes@gmail.com>
2013-12-30 23:19:48 +0900

    update .gitignore

---
+++ files/.emacs.d/elpa/popup-20131230.407/popup-autoloads.el
@@ -0,0 +1,18 @@
+;;; popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("popup-pkg.el" "popup.el") (21185 32784
+;;;;;;  939044 158000))
+
+;;;***
+
+(provide 'popup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; popup-autoloads.el ends here
