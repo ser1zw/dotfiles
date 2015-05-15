cc09b6e761cd1c723b388d9b14e71a6401232773
ser1zw <azariahsawtikes@gmail.com>
2015-05-15 21:04:25 +0900

    change exec-path-from-shell

--- files/.emacs.d/config/10-exec-path-from-shell.el
+++ files/.emacs.d/config/10-exec-path-from-shell.el
@@ -1,15 +1,16 @@
 ;; -*- mode: emacs-lisp; coding: utf-8 -*-
 
-;; (require 'exec-path-from-shell)
-;; (exec-path-from-shell-initialize)
+(add-to-list 'load-path (get-latest-package-dir "exec-path-from-shell"))
+(require 'exec-path-from-shell)
+(exec-path-from-shell-initialize)
 
 ;; (let ((envs '("PATH")))
 ;;   (exec-path-from-shell-copy-envs envs))
 
 ;; http://kotatu.org/blog/2012/03/02/emacs-path-settings/
-(let ((path-str
-       (replace-regexp-in-string
-	"\n+$" "" (shell-command-to-string "echo $PATH"))))
-  (setenv "PATH" path-str)
-  (setq exec-path (nconc (split-string path-str ":") exec-path)))
+;; (let ((path-str
+;;        (replace-regexp-in-string
+;; 	"\n+$" "" (shell-command-to-string "echo $PATH"))))
+;;   (setenv "PATH" path-str)
+;;   (setq exec-path (nconc (split-string path-str ":") exec-path)))
 
