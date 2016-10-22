366772fe7cd87c53cb03b6fa42cfe9c24ff88129
ser1zw <azariahsawtikes@gmail.com>
2016-10-23 04:03:48 +0900

    update emacs packages

--- files/.emacs.d/elpa/actionscript-mode-20140605.1128/actionscript-mode-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; actionscript-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "actionscript-mode" "actionscript-mode.el"
-;;;;;;  (21575 54443 100942 771000))
-;;; Generated autoloads from actionscript-mode.el
-
-(autoload 'actionscript-mode "actionscript-mode" "\
-Major mode for editing Actionscript files.
-\\{actionscript-mode-map}
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.as$" . actionscript-mode))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; actionscript-mode-autoloads.el ends here
