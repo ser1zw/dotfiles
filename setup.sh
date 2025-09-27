eeb568e2fc34642948b1d6e803b6f645cf206277
ser1zw <azariahsawtikes@gmail.com>
2025-09-28 06:34:49 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,10 +11,9 @@ DOT_FILES=`ls -a $HOME/dotfiles/files | grep -v '^\.\{1,2\}$'`
 
 for file in ${DOT_FILES[@]}
 do
-    if [ -e $HOME/$file ]; then
-	echo -e $RED"$HOME/$file already exists. Skipped."
-    else
-	ln -s $HOME/dotfiles/files/$file $HOME/$file && echo -e $GREEN"$HOME/$file successfully created."
-    fi
+  if [ -e $HOME/$file ]; then
+    echo -e $RED"$HOME/$file already exists. Skipped."
+  else
+    ln -s $HOME/dotfiles/files/$file $HOME/$file && echo -e $GREEN"$HOME/$file successfully created."
+  fi
 done
-
